a12c17ccf50bfea941ce3b2a92c4e8de6c6e4a7e
Richard Zhou <richardzhou163@gmail.com>
2025-03-24 11:29:25 -0400

    Modified Program: threads process a chunk of password from the password file (divided equally amongst the threads) to reduce overhead

--- src/hash.c
+++ src/hash.c
@@ -4,117 +4,14 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <pthread.h>
-
 #include "hash_functions.h"
 
 #define KEEP 16 // only the first 16 bytes of a hash are kept
+#define MAX_PASSWORD_CHARACTER 256
+#define MAX_NUM_PASSWORD 999998
+#define NUM_THREADS 4
 
-// TODO: Implement a simple queue data structure
-// This queue will contain passwords from the password file
-/*
-*************************************************************
-**                 Queue Implementation
-*************************************************************
-*/
-typedef struct node_t {
-    char password[256];
-    struct node_t* next;
-} Node;
-
-typedef struct queue_t {
-    Node *front, *rear;
-} Queue;
-
-// Initialize a new node
-Node* create_node(char password[256]) {
-    Node* new_node = malloc(sizeof(Node));
-    strcpy(new_node->password, password);
-    new_node->next = NULL;
-    return new_node;
-}
-
-// Initialize a new queue
-Queue* create_queue() {
-    Queue* q = malloc(sizeof(Queue));
-    q->front = q->rear = NULL;
-    return q;
-}
-
-// Checks if the queue is empty
-bool is_empty(Queue* q) {
-    // If both the front and rear pointer point to NULL, the queue is empty
-    if (q->front == NULL) {
-        return true;
-    }
-
-    return false;
-}
-
-// Insert element to the end of queue
-void enqueue(Queue* q, char password[256]) {
-    Node* new_node = create_node(password);
-
-    // If the queue is empty, initialize the head of the queue and tail of the queue to the new node
-    if (is_empty(q)) {
-        q->front = q->rear = new_node;
-        return;
-    }
-
-    // Otherwise, add the new node at the end of the queue
-    q->rear->next = new_node;
-    q->rear = new_node;
-}
-
-// Pop the front of the queue
-void dequeue(Queue* q) {
-    // If the queue is empty, simply return
-    if (is_empty(q)) return;
-
-    Node* temp = q->front;
-    q->front = q->front->next;
-
-    // If the front of queue is empty, queue is empty, set rear of queue to NULL
-    if (q->front == NULL) {
-        q->rear = NULL;
-    }
-
-    // Free allocated resource
-    free(temp);
-}
-
-char* get_front(Queue* q) {
-    // Check to see if the queue is empty
-    if (is_empty(q)) {
-        printf("Queue is empty.\n");
-        return NULL;
-    }
-
-    return q->front->password;
-}
-
-char* get_rear(Queue* q) {
-    // Check to see if the queue is empty
-    if (is_empty(q)) {
-        printf("Queue is empty.\n");
-        return NULL;
-    }
-
-    return q->rear->password;
-}
-
-// Debug function, to check what's in the queue
-void print_queue(Queue* q) {
-    Node* current = q->front;
-
-    int count = 0;
-    while (current != NULL) {
-        current = current->next;
-        count++;
-    }
-    printf("Processed %d passwords.\n", count);
-}
-
-/************************************************************/
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 struct cracked_hash {
 	char hash[2*KEEP+1];
@@ -134,38 +31,27 @@ int compare_hashes(char *a, char *b) {
 	return 1;
 }
 
-pthread_mutex_t queue_lock; // mutex
-
+// Set up thread argument strucutre
 typedef struct arg {
-    Queue* shared_queue;
+    char** passwords;
+    int start_index;
+    int end_index;
     struct cracked_hash* cracked_hashes;
     int num_hashes;
 } thread_args_t;
 
-// This will be the function to run on threads
-// Each thread will pull passwords from the queue and work on cracking them
-void* worker_crack_password(void* arg) {
+// This thread function will work on a split chunk of the password file
+void* thread_crack_password(void* arg) {
     thread_args_t* args = (thread_args_t *) arg;
     char hex_hash[2*KEEP+1];
 
-    while (1) {
-        pthread_mutex_lock(&queue_lock);
+    // Each thread goes through each of their workload of passwords
+    for(int idx = args->start_index; idx < args->end_index; idx++) {
+        char* password = args->passwords[idx];
 
-        if (is_empty(args->shared_queue)) {
-            pthread_mutex_unlock(&queue_lock);
-            break;
-        }
-
-        // Fetch password from queue
-        char* password = get_front(args->shared_queue);
-        char password_copy[256];
-        strcpy(password_copy, password);  // Create a local copy
-        dequeue(args->shared_queue);
-
-        pthread_mutex_unlock(&queue_lock);
-
+        // Use all four hashing algorithms on password to find matches
         for (int i = 0; i < n_algs; i++) {
-            unsigned char* hash = fn[i]((unsigned char *) password_copy, strlen(password_copy));
+            unsigned char* hash = fn[i]((unsigned char *) password, strlen(password));
 
             // Format the entire hash up to KEEP bytes
             for (int j = 0; j < KEEP; j++) {
@@ -174,30 +60,31 @@ void* worker_crack_password(void* arg) {
             hex_hash[2*KEEP] = '\0';  // Ensure null termination
 
             for (int j = 0; j < args->num_hashes; j++) {
-                if (args->cracked_hashes[j].password != NULL)
+                if (args->cracked_hashes[j].password != NULL) {
                     continue;
+                }
                 if (compare_hashes(hex_hash, args->cracked_hashes[j].hash)) {
-                    args->cracked_hashes[j].password = strdup(password_copy);
-                    args->cracked_hashes[j].alg = algs[i];
+                    pthread_mutex_lock(&lock);
+                    if (args->cracked_hashes[j].password == NULL) {
+                        args->cracked_hashes[j].password = strdup(password);
+                        args->cracked_hashes[j].alg = algs[i];
+                    }
+                    pthread_mutex_unlock(&lock);
                     break;
                 }
             }
         }
     }
+
     return NULL;
 }
-// Need a condition variable to signal the thread to do something when the queue is not empty
-// Need a lock on the shared queue since it is a shared resource
+
 void crack_hashed_passwords(char *password_list, char *hashed_list, char* output) {
 	FILE *fp;
-	char password[256];  // passwords have at most 255 characters
+	char password_buffer[MAX_PASSWORD_CHARACTER];  // passwords have at most 255 characters
 	char hex_hash[2*KEEP+1]; // hashed passwords have at most 'keep' characters
 
-    // Initialize the shared queue and the lock
-    Queue* shared_queue = create_queue();
-    pthread_mutex_init(&queue_lock, NULL);
-
-	// load hashed passwords
+    // Read hashes from hashed list
 	int n_hashed = 0;
 	struct cracked_hash *cracked_hashes;
 	fp = fopen(hashed_list, "r");
@@ -214,45 +101,38 @@ void crack_hashed_passwords(char *password_list, char *hashed_list, char* output
 	}
 	fclose(fp);
 
-    // Load passwords from the password file, and add them to shared queue
+    // Read passwords from password list
 	fp = fopen(password_list, "r");
 	assert(fp != NULL);
 
-	while(fscanf(fp, "%s", password) == 1) {
-        enqueue(shared_queue, password);
+    char* passwords[MAX_NUM_PASSWORD];
+    int num_passwords = 0;
+	while(fscanf(fp, "%s", password_buffer) == 1) {
+        passwords[num_passwords] = strdup(password_buffer);
+        num_passwords++;
 	}
 	fclose(fp);
 
-    // NOTE: Password added successfully
-    char* front = get_front(shared_queue);
-
-    // TODO: Now that assume all passwords have been added to the shared queue, need threads to start working
-    // FIXME: Currently this functionality is not working
-    // Set up thread argument structure
-    thread_args_t* arg = malloc(sizeof(thread_args_t));
-    arg->cracked_hashes = cracked_hashes;
-    arg->num_hashes = n_hashed;
-    arg->shared_queue = shared_queue;
-
     // Create threads
-    pthread_t th[4];
-    for (int i = 0; i < 4; i++) {
-        if(pthread_create(&th[i], NULL, &worker_crack_password, (void *) arg) != 0) {
-            perror("Failed to create threads.\n");
-        };
+    pthread_t th[NUM_THREADS];
+    thread_args_t *arg[NUM_THREADS];
+    int chunk_size = num_passwords / NUM_THREADS;
+    for (int i = 0; i < NUM_THREADS; i++) {
+        arg[i] = malloc(sizeof(thread_args_t));
+        arg[i]->passwords = passwords;
+        arg[i]->start_index = i * chunk_size;
+        arg[i]->end_index = (i == NUM_THREADS - 1) ? num_passwords : (i + 1) * chunk_size;
+        arg[i]->cracked_hashes = cracked_hashes;
+        arg[i]->num_hashes = n_hashed;
+        pthread_create(&th[i], NULL, thread_crack_password, arg[i]);
     }
 
     // Join threads
-    for (int i = 0; i < 4; i++) {
-        if (pthread_join(th[i], NULL) != 0) {
-            perror("Failed to join threads.\n");
-        }
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(th[i], NULL);
     }
 
-    // Destory the lock
-    pthread_mutex_destroy(&queue_lock);
-
-    // print results
+    // Write final results to output file
 	fp = fopen(output, "w");
 	assert(fp != NULL);
 	for(int i=0; i < n_hashed; i++) {
@@ -263,7 +143,13 @@ void crack_hashed_passwords(char *password_list, char *hashed_list, char* output
 	}
 	fclose(fp);
 
-	// release stuff
+	// Clean up allocated resources
+    for (int i = 0; i < num_passwords; i++) {
+        free(passwords[i]);
+    }
+    for (int i = 0; i < NUM_THREADS; i++) {
+        free(arg[i]);
+    }
 	for(int i=0; i < n_hashed; i++)
 		free(cracked_hashes[i].password);
 	free(cracked_hashes);
